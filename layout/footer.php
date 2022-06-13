dbe83b51d0f1efeb09d0ed6d634c96fd3217bd4e
mackliu <mackliu@gmail.com>
2022-06-13 11:57:31 +0800

    建立各功能資料夾及版面規劃

---
+++ layout/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <div class="right">泰山投票中心版權所有</div>
+</footer>
---
+++ layout/front_nav.php
@@ -0,0 +1,5 @@
+<nav>
+        <a href="index.php">回首頁</a>
+        <a href="index.php">登入</a>
+        <a href="index.php">投票列表</a>
+    </nav>
