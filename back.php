8e6c0a928ce3de669893abb1a22a4fa207adc303
mackliu <mackliu@gmail.com>
2022-06-17 14:06:59 +0800

    強化後台主題列表的呈現方式，增加必要的欄位，並且使用CSS加以美化

--- back.php
+++ back.php
@@ -35,6 +35,14 @@ if(isset($file) && file_exists($file)){
     
     <div>
         <ul>
+            <li class='list-header'>
+                <div>投票主題</div>
+                <div>單/複選題</div>
+                <div>投票期間</div>
+                <div>剩餘天數</div>
+                <div>投票人數</div>
+                <div>操作</div>
+            </li>
         <?php
             //使用all()函式來取得資料表subjects中的所有資料，請參考base.php中的函式all($table,...$arg)
             $subjects=all('subjects');
@@ -42,9 +50,31 @@ if(isset($file) && file_exists($file)){
             //使用迴圈將每一筆資料的內容顯示在畫面上
             foreach($subjects as $subject){
                 echo "<li class='list-items'>";
-                echo $subject['subject'];
+                echo "<div>{$subject['subject']}</div>";
+                if($subject['multiple']==0){
+                    echo "<div class='text-center'>單選題</div>";
+                }else{
+                    echo "<div class='text-center'>複選題</div>";
+                }
+                echo "<div class='text-center'>";
+                echo $subject['start']. " ~ ".$subject['end'];
+                echo "</div>";
+                echo "<div class='text-center'>";
+                    $today=strtotime("now");
+                    $end=strtotime($subject['end']);
+                    if(($end-$today)>0){
+                        $remain=floor(($end-$today)/(60*60*24));
+                        echo "倒數".$remain."天結束";
+                    }else{
+                        echo "<span style='color:grey'>投票已結束</span>";
+                    }
+                
+                echo "</div>";
+                echo "<div class='text-center'>{$subject['total']}</div>";
+                echo "<div class='text-center'>";
                 echo "<a class='edit' href='?do=edit&id={$subject['id']}'>編輯</a>";
                 echo "<a class='del' href='?do=del&id={$subject['id']}'>刪除</a>";
+                echo "</div>";
                 echo "</li>";
             }
 
