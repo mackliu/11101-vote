1d5f3e20141868a700ff805dc4f7e91182fc656f
mackliu <mackliu@gmail.com>
2022-06-20 10:47:21 +0800

    使用css 來產生長條圖效果

--- front/vote_result.php
+++ front/vote_result.php
@@ -4,9 +4,8 @@ $opts=all("options",['subject_id'=>$_GET['id']]);
 ?>
 <h1 class="text-center"><?=$subject['subject'];?></h1>
 <div style="width:600px;margin:auto">
-
     <div>總投票數:<?=$subject['total'];?></div>
-    <table>
+    <table class="result-table">
         <tr>
             <td>選項</td>
             <td>投票數</td>
@@ -15,16 +14,20 @@ $opts=all("options",['subject_id'=>$_GET['id']]);
         <?php 
         foreach($opts as $opt){
             $total=($subject['total']==0)?1:$subject['total'];
+            $rate=$opt['total']/$total;
         ?>
         <tr>
             <td><?=$opt['option'];?></td>
             <td><?=$opt['total'];?></td>
-            <td><?=$opt['total']/$total;?></td>
+            <td>
+                <div style="display:inline-block;height:24px;background:skyblue;width:<?=300*$rate;?>px;"></div>
+                <?=($rate*100) . "%";?>
+            </td>
         </tr>
         <?php 
 
         }
         ?>
     </table>
-    <button onclick="location.href='?do=vote&id=<?=$_GET['id'];?>'">我要投票</button>
+    <button class="btn btn-info" onclick="location.href='?do=vote&id=<?=$_GET['id'];?>'">我要投票</button>
 </div>
